2257b046ef381d4cdbf2fe875027d7a07eb97d2b
Alan Fernandes <allanfernds@gmail.com>
2023-07-04 17:33:35 -0300

    feat: create ProfileCheck.jsx

---
+++ src/components/ProfileCheck.jsx
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useUserAuth } from '../context/UserAuthContext';
+
+const ProfileCheck = () => {
+  const { user } = useUserAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Verifica se o usuário está autenticado e possui nome de usuário e imagem de perfil
+    if (user && user.displayName && user.photoURL) {
+      console.log(user.displayName, user.photoURL);
+      navigate('/home'); // Redireciona para a página inicial
+    } else {
+      navigate('/update-user-info'); // Redireciona para o componente de atualização de informações
+    }
+  }, [user, navigate]);
+
+  return null;
+};
+
+export default ProfileCheck;
