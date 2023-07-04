657045bc92487a1460e1f47bc43d5de9eb96301c
Alan Fernandes <allanfernds@gmail.com>
2023-07-04 09:48:15 -0300

    feat: create UserAuthContext.jsx and firebase.user.auth.js

---
+++ src/context/UserAuthContext.jsx
@@ -0,0 +1,57 @@
+/* eslint-disable react/prop-types */
+import { createContext, useContext, useEffect, useState } from 'react';
+import {
+  onAuthStateChanged,
+  signOut,
+  RecaptchaVerifier,
+  signInWithPhoneNumber,
+} from 'firebase/auth';
+import { auth } from '../firebase';
+
+const userAuthContext = createContext();
+
+export function UserAuthContextProvider({ children }) {
+  const [user, setUser] = useState({});
+
+  function logOut() {
+    return signOut(auth);
+  }
+
+  function setUpRecaptha(number) {
+    const recaptchaVerifier = new RecaptchaVerifier(
+      'recaptcha-container',
+      {},
+      auth
+    );
+    recaptchaVerifier.render();
+    return signInWithPhoneNumber(auth, number, recaptchaVerifier);
+  }
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentuser) => {
+      console.log('Auth', currentuser);
+      setUser(currentuser);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  return (
+    <userAuthContext.Provider
+      value={{
+        user,
+        logOut,
+        setUpRecaptha,
+      }}
+    >
+      {children}
+    </userAuthContext.Provider>
+  );
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export function useUserAuth() {
+  return useContext(userAuthContext);
+}
---
+++ src/services/firebase/firebase.user.auth.js
@@ -0,0 +1,38 @@
+import {
+  RecaptchaVerifier,
+  getAuth,
+  signInWithPhoneNumber,
+} from 'firebase/auth';
+
+const auth = getAuth();
+
+window.recaptchaVerifier = new RecaptchaVerifier(
+  'sign-in-button',
+  {
+    size: 'invisible',
+    callback: (response) => {
+      console.log(response);
+      // reCAPTCHA solved, allow signInWithPhoneNumber.
+    },
+  },
+  auth
+);
+
+const appVerifier = window.recaptchaVerifier;
+
+const userSingInWithPhoneNumber = async (phoneNumber) => {
+  signInWithPhoneNumber(auth, phoneNumber, appVerifier)
+    .then((confirmationResult) => {
+      // SMS sent. Prompt user to type the code from the message, then sign the
+      // user in with confirmationResult.confirm(code).
+      window.confirmationResult = confirmationResult;
+      // ...
+    })
+    .catch((error) => {
+      console.log(error);
+      // Error; SMS not sent
+      // ...
+    });
+};
+
+export default userSingInWithPhoneNumber;
