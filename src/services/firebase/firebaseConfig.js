c1ce639711698a412e92689295dc9162cdc91225
Alan Fernandes <allanfernds@gmail.com>
2023-07-04 17:32:47 -0300

    update firebaseConfig.js create 1 file and update 1 file

--- src/services/firebase/firebaseConfig.js
+++ src/services/firebase/firebaseConfig.js
@@ -1,5 +1,6 @@
 
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // import { getAnalytics } from "firebase/analytics";
 
@@ -15,10 +16,8 @@ const firebaseConfig = {
 
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export default app
 // const analytics = getAnalytics(app);
 
-export {
-  app, 
-  db
-}
