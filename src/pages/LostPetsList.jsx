fa7144db63dd36b65baa18877b8dbfde74b8e323
Alan Fernandes <allanfernds@gmail.com>
2023-07-06 18:57:42 -0300

    update LostPetsList.jsx

--- src/pages/LostPetsList.jsx
+++ src/pages/LostPetsList.jsx
@@ -1,24 +1,33 @@
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import LostPetCard from '../components/PetCard';
 import { listAllLostPets } from '../services/firebase/firestore.functions';
 
 function LostPetsList() {
   const [pets, setPets] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchLostPets() {
       const lostPets = await listAllLostPets();
       setPets(lostPets);
+      setLoading(false);
     }
 
     fetchLostPets();
   }, []);
 
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
       <h2>Lost Pets</h2>
       {pets.map((pet, index) => (
-        <LostPetCard key={index} pet={pet} />
+        <Link key={index} to={`/lost-pets/${pet.id}`}>
+          <LostPetCard pet={pet} />
+        </Link>
       ))}
     </div>
   );
