df78bbc561dc79725bb224fce8e18a73de51c243
Alan Fernandes <allanfernds@gmail.com>
2023-07-08 05:23:11 -0300

    remove logOut btn

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,13 @@
 import NavBar from '../components/NavBar';
-import { useUserAuth } from '../context/UserAuthContext';
+
 import { Link } from 'react-router-dom';
 import LostPetsList from './LostPetsList';
 
 function Home() {
-  const { logOut } = useUserAuth();
-
   return (
     <div>
       <NavBar />
-      <button onClick={logOut}>Sair</button>
-      <hr />
+
       <Link to="/create-lost-pet">
         <button>Perdi meu pet</button>
       </Link>
