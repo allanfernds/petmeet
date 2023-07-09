46f6d324e95a5ab3e4c274cc5ab172c170fd7bff
Alan Fernandes <allanfernds@gmail.com>
2023-07-09 18:49:59 -0300

    style: create pet form

--- src/components/CreateLostPetForm.jsx
+++ src/components/CreateLostPetForm.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { createLostPet } from '../services/firebase/firestore.functions';
 import { useUserAuth } from '../context/UserAuthContext';
+import NavBar from './NavBar';
 
 function CreateLostPetForm() {
   const { user } = useUserAuth();
@@ -54,101 +55,151 @@ function CreateLostPetForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Name:
-        <input
-          type="text"
-          name="name"
-          value={pet.name}
-          onChange={handleInputChange}
-        />
-      </label>
-      <label>
-        Type:
-        <select name="type" value={pet.type} onChange={handleInputChange}>
-          <option value="">Selecione um tipo</option>
-          <option value="Cachorro">Cachorro</option>
-          <option value="Gato">Gato</option>
-        </select>
-      </label>
-      <label>
-        Breed:
-        <input
-          type="text"
-          name="breed"
-          value={pet.breed}
-          onChange={handleInputChange}
-        />
-      </label>
-      <label>
-        Description:
-        <textarea
-          name="description"
-          value={pet.description}
-          onChange={handleInputChange}
-        />
-      </label>
-      <label>
-        Location:
-        <input
-          type="text"
-          name="location"
-          value={pet.location}
-          onChange={handleInputChange}
-        />
-      </label>
-      <label>
-        Last Seen Date:
-        <input
-          type="text"
-          name="lastSeenDate"
-          value={pet.lastSeenDate}
-          onChange={handleInputChange}
-        />
-      </label>
-      <label>
-        Contact Name:
-        <input
-          type="text"
-          name="contact.name"
-          value={pet.contact.name}
-          onChange={handleInputChange}
-        />
-      </label>
-      <label>
-        Contact Email:
-        <input
-          type="email"
-          name="contact.email"
-          value={pet.contact.email}
-          onChange={handleInputChange}
-        />
-      </label>
-      <label>
-        Contact Phone:
-        <input
-          type="text"
-          name="contact.phone"
-          value={pet.contact.phone}
-          onChange={handleInputChange}
-        />
-      </label>
-      <label>
-        Offer Reward:
-        <input
-          type="checkbox"
-          name="offerReward"
-          checked={pet.offerReward}
-          onChange={handleInputChange}
-        />
-      </label>
-      <label>
-        Image:
-        <input type="file" accept="image/*" onChange={handleImageChange} />
-      </label>
-      <button type="submit">Create Lost Pet</button>
-    </form>
+    <>
+      <NavBar />
+      <form
+        onSubmit={handleSubmit}
+        className="max-w-xs h-full mx-auto space-y-4 mt-20 mb-20"
+      >
+        <div className="flex flex-col mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Nome do seu Pet:
+          </label>
+          <input
+            placeholder="Ex: Melbi"
+            type="text"
+            name="name"
+            value={pet.name}
+            onChange={handleInputChange}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className="text-gray-700">Cachorro ou Gato?</label>
+          <select
+            name="type"
+            value={pet.type}
+            onChange={handleInputChange}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          >
+            <option value="">Selecione um tipo</option>
+            <option value="Cachorro">Cachorro</option>
+            <option value="Gato">Gato</option>
+          </select>
+        </div>
+        <div className="flex flex-col">
+          <label className="text-gray-700">Qual raça?</label>
+          <input
+            placeholder="Ex: Rottweiler"
+            type="text"
+            name="breed"
+            value={pet.breed}
+            onChange={handleInputChange}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className="text-gray-700">
+            Descreva seu{' '}
+            <strong className="font-semibold text-green-500">au</strong>migo ou
+            <strong className="font-semibold text-green-500"> miau</strong>migo
+          </label>
+          <textarea
+            placeholder="Descreva caracteristicas do seu pet"
+            name="description"
+            value={pet.description}
+            onChange={handleInputChange}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className="text-gray-700">
+            Onde ele foi visto pela ultima vez?
+          </label>
+          <input
+            placeholder="Ex: Bairro Feira X"
+            type="text"
+            name="location"
+            value={pet.location}
+            onChange={handleInputChange}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className="text-gray-700">
+            Quando ele foi visto pela ultima vez?
+          </label>
+          <input
+            placeholder="Ex: 01/02/2003"
+            type="text"
+            name="lastSeenDate"
+            value={pet.lastSeenDate}
+            onChange={handleInputChange}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className="text-gray-700">Nome do tutor</label>
+          <input
+            placeholder="Ex: Camila Gonçalves"
+            type="text"
+            name="contact.name"
+            value={pet.contact.name}
+            onChange={handleInputChange}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className="text-gray-700">Contact Email:</label>
+          <input
+            placeholder="Ex: email@mail.com"
+            type="email"
+            name="contact.email"
+            value={pet.contact.email}
+            onChange={handleInputChange}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className="text-gray-700">Contact Phone:</label>
+          <input
+            placeholder="+55 7598892-7560"
+            type="text"
+            name="contact.phone"
+            value={pet.contact.phone}
+            onChange={handleInputChange}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="flex items-center">
+          <label className="flex items-center text-gray-700">
+            <input
+              type="checkbox"
+              name="offerReward"
+              checked={pet.offerReward}
+              onChange={handleInputChange}
+              className="mr-2"
+            />
+            <span>Quer oferecer uma recompensa?</span>
+          </label>
+        </div>
+        <div className="flex flex-col">
+          <label className="text-gray-700">Image:</label>
+          <input
+            type="file"
+            accept="image/*"
+            onChange={handleImageChange}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <button
+          type="submit"
+          className="bg-green-500 text-white py-2 px-4 rounded-md shadow-sm hover:bg-green-600"
+        >
+          Create Lost Pet
+        </button>
+      </form>
+    </>
   );
 }
 
