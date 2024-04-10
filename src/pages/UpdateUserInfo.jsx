77ebfc33347351b4ca6e014e4767c9603294a34d
Alan Fernandes <allanfernds@gmail.com>
2024-04-10 08:01:21 -0300

    update UpdateUserInfo.jsx

--- src/pages/UpdateUserInfo.jsx
+++ src/pages/UpdateUserInfo.jsx
@@ -81,7 +81,7 @@ const UpdateUserInfo = () => {
             type="submit"
             className="py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600"
           >
-            Update
+            Atualizar
           </button>
         </form>
       </div>
