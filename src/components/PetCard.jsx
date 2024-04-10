a7b549ea2161cbb4981e8442249984bad1b8f5b5
Alan Fernandes <allanfernds@gmail.com>
2024-04-10 08:04:47 -0300

    componentizando e mudando aparencia

--- src/components/PetCard.jsx
+++ src/components/PetCard.jsx
@@ -7,8 +7,19 @@ import {
   FaCat,
   FaDog,
 } from 'react-icons/fa';
+import { motion } from 'framer-motion';
 
 import { formatString } from '../utils';
+
+function PetInfo({ infoTitle, petInfo, icon }) {
+  return (
+    <div className="flex items-center text-sky-500">
+      {icon}
+      <strong className="text-sky-500 mx-2">{infoTitle}</strong>
+      <p className="text-gray-600 flex-grow">{petInfo}</p>
+    </div>
+  );
+}
 
 function LostPetCard({ pet }) {
   const cardStyle = {
@@ -27,45 +38,50 @@ function LostPetCard({ pet }) {
   };
 
   return (
-    <div className="rounded-lg overflow-hidden shadow-md my-5 w-[370px] lg:w-[70%]">
-      {pet.offerReward && (
-        <div className="bg-yellow-400 text-yellow-800 text-center px-2 py-1">
-          <FaCoins className="inline-block mr-1" />
-          Ofereço recompensa
-        </div>
-      )}
+    <motion.div
+      transition={{
+        ease: 'linear',
+        duration: 2,
+        x: { duration: 1 },
+      }}
+      className="rounded-lg overflow-hidden shadow-md my-5 w-[370px] lg:w-[330px] "
+    >
       <div className="w-full">
         <div style={cardStyle} className="h-64"></div>
         <div className="p-4 bg-white">
           <div className="flex items-center">
             {getPetIcon()}
             <h3 className="text-2xl font-bold text-gray-7a00">{pet.name}</h3>
-            <strong className="ml-2  font-semibold text-center text-xs text-white p-1 rounded-md bg-green-500">
+            <strong className="ml-2  font-semibold text-center text-xs text-white p-1 rounded-md bg-sky-500">
               {formatString(pet.breed)}
             </strong>
           </div>
         </div>
+        {pet.offerReward && (
+          <div className=" font-semibold text-center text-xs text-white p-1  bg-yellow-500">
+            <FaCoins className="inline-block mr-1" />
+            Ofereço Recompensa
+          </div>
+        )}
       </div>
       <div className="p-3 bg-white space-y-1">
-        <div className="flex items-center">
-          <FaUserAlt className="text-green-500 mr-2" />
-          <strong className="text-green-500 mr-2">Tutor</strong>
-          <p className="text-gray-600 flex-grow">{pet.contact.name}</p>
-        </div>
-        <div className="flex items-center">
-          <FaMapMarkerAlt className="text-green-500 mr-2" />
-          <strong className="text-green-500 mr-2">Último local visto</strong>
-          <p className="text-gray-600 flex-grow">
-            {formatString(pet.location)}
-          </p>
-        </div>
-        <div className="flex items-center">
-          <FaCalendarAlt className="text-green-500 mr-2" />
-          <strong className="text-green-500 mr-2">Data em que sumiu</strong>
-          <p className="text-gray-600 flex-grow">{pet.lastSeenDate}</p>
-        </div>
+        <PetInfo
+          infoTitle="Tutor"
+          petInfo={pet.contact.name}
+          icon={<FaUserAlt />}
+        />
+        <PetInfo
+          infoTitle="Último local visto"
+          petInfo={pet.location}
+          icon={<FaMapMarkerAlt />}
+        />
+        <PetInfo
+          infoTitle="Desapareceu na data"
+          petInfo={pet.lastSeenDate.split('-').reverse().join('/')}
+          icon={<FaCalendarAlt />}
+        />
       </div>
-    </div>
+    </motion.div>
   );
 }
 
