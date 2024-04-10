20642a2281058d9f5e6ea864ca60f95a3a00cf6b
Alan Fernandes <allanfernds@gmail.com>
2024-04-10 08:03:45 -0300

    adicionando o provider de auth

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { PetsProvider } from './context/PetsContext.jsx';
+import { UserAuthContextProvider } from './context/UserAuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <PetsProvider>
-      <App />
-    </PetsProvider>
+    <UserAuthContextProvider>
+      <PetsProvider>
+        <App />
+      </PetsProvider>
+    </UserAuthContextProvider>
   </BrowserRouter>
 );
